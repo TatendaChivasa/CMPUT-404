f065d8c42b49508ada87ddd7621a7321465e5042
TATENDA CHIVASA <tatenda@TATENDAs-MacBook-Pro.local>
2021-01-16 00:27:14 +0200

    added download component

--- cmput404_lab1/script.py
+++ cmput404_lab1/script.py
@@ -6,3 +6,7 @@ print(requests.__version__)
 #getting the google homepage
 response = requests.get("http://google.com/")
 print(response.status_code)
+
+#getting script from github
+script_file = requests.get("https://raw.githubusercontent.com/TatendaChivasa/CMPUT-404/main/cmput404_lab1/script.py")
+print(script_file.text)
